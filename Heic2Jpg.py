782589bbffd4ab6be4da7075ca9b898a6c4005d0
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2023-02-16 15:53:20 +0100

    upd

--- Heic2Jpg.py
+++ Heic2Jpg.py
@@ -1,14 +1,17 @@
 import os
 from PIL import Image
 import pillow_heif
+import sys
+import argparse
 
 # format png, jpeg
 
 class Convert:
 
-    def __init__(self, src:str, target='converted', format='jpeg'):
+    def __init__(self, src:str, target='converted', format='jpeg', save=True):
 
         self.src = os.path.join(os.getcwd(), src)
+        self.save = save
         self.target = os.path.join(os.getcwd(), target)
         self.format = format
 
@@ -25,13 +28,11 @@ class Convert:
     def __readData(self):
 
         data = []
-        if not os.path.isdir(self.src):
-            raise Exception('{0} is not directory'.format(self.src))
-        
-        for filename in os.listdir(self.src):
-            if filename.endswith(".heic") or filename.endswith(".HEIC"):
-                absolute_filename = os.path.join(self.src, filename)
-                data.append(absolute_filename)
+        if os.path.isdir(self.src):        
+            for filename in os.listdir(self.src):
+                if filename.endswith(".heic") or filename.endswith(".HEIC"):
+                    absolute_filename = os.path.join(self.src, filename)
+                    data.append(absolute_filename)
 
         return data
     
@@ -40,13 +41,65 @@ class Convert:
         print('start convert')
         if not os.path.exists(self.target):
             os.mkdir(self.target)
-            
+
         for img in toConvert:
-            newFileName = os.path.join(self.target, os.path.basename(img).split('.')[0] + f".{self.format}")           
-            heif_file = pillow_heif.read_heif(img)
-            image = Image.frombytes( heif_file.mode, heif_file.size, heif_file.data, "raw")
-            image.save(newFileName, format=self.format)
+            newFileName = os.path.join(self.target, os.path.basename(img).split('.')[0] + f".{self.format}")  
+            if self.save:         
+                heif_file = pillow_heif.read_heif(img)
+                image = Image.frombytes( heif_file.mode, heif_file.size, heif_file.data, "raw")
+                image.save(newFileName, format=self.format)
+            else:
+                print(f'{img} -> {newFileName}')
 
-        print('end convert')
+        print('end convert', f'save {self.save}')
 
+def main():
+    
+    print('main')
 
+    try:
+
+        format = 'jpeg'
+        src = 'fotos'
+        target = 'converted'
+        save = False
+
+        parser = argparse.ArgumentParser(
+                        prog = 'imageConverter',
+                        description='convert image from .heic',             
+        )
+
+        parser.add_argument('-src', type=str, required=False)
+        parser.add_argument('-target', type=str, required=False)
+        parser.add_argument('-format', type=str, required=False)
+        parser.add_argument('-save', action='store_true')
+        args = parser.parse_args()
+
+    #print(args)
+        if args.src:
+            src = args.src
+
+        if args.target:
+            target = args.target
+
+        if args.format:
+            format = args.format
+
+        if args.save:
+            save = bool(args.save)
+
+        print('src:', src)
+        print('target:', target)
+        print('format:', format)
+        print('save:', save)   
+
+        conv = Convert(src=src, target=target, format=format, save=save)
+        conv.processData()
+
+    except KeyError as err:
+        print('wrong key ->', err)
+    except Exception as err:
+        print('ERROR ->', err)
+
+if __name__ == '__main__':
+    main()
